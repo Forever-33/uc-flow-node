0fb4efbb2e054d6ee278e5657a5ba14913e44016
Forever-33 <ivan110370@mail.ru>
2023-12-01 18:24:08 +0000

    update ICON

--- node/service.py
+++ node/service.py
@@ -6,6 +6,7 @@ from uc_flow_nodes.service import NodeService
 from uc_flow_nodes.views import info, execute
 from uc_flow_schemas import flow
 from uc_flow_schemas.flow import Property, CredentialProtocol, RunState
+from node.static.icon import ICON
 
 
 class NodeType(flow.NodeType):
@@ -14,7 +15,7 @@ class NodeType(flow.NodeType):
     name: str = 'uc-flow-node'
     is_public: bool = False
     displayName: str = 'uc-flow-node'
-    icon: str = '<svg><text x="8" y="50" font-size="50">😎</text></svg>'
+    icon: str = ICON
     description: str = 'uc-flow-node service'
     properties: List[Property] = [
         Property(
@@ -38,7 +39,7 @@ class NodeType(flow.NodeType):
         Property(
             displayName='number/text',
             name='return_type',
-            type=Property.Type.BOOLEAN, 
+            type=Property.Type.BOOLEAN,
             placeholder='Return type placeholder',
             description='Return type description',
             required=True,
@@ -118,14 +119,17 @@ class ExecuteView(execute.Execute):
     async def post(self, json: NodeRunContext) -> NodeRunContext:
         try:
             switch_field_value = json.node.data.properties['switch_field']
-            dropdown_field_1_value = json.node.data.properties.get('dropdown_field_1')
-            dropdown_field_2_value = json.node.data.properties.get('dropdown_field_2')
+            dropdown_field_1_value = json.node.data.properties.get(
+                'dropdown_field_1')
+            dropdown_field_2_value = json.node.data.properties.get(
+                'dropdown_field_2')
             email_field_value = json.node.data.properties.get('email_field')
-            datetime_field_value = json.node.data.properties.get('datetime_field')
+            datetime_field_value = json.node.data.properties.get(
+                'datetime_field')
             text_value = json.node.data.properties['first_text_field']
             number_value = json.node.data.properties['first_number_field']
             return_type = json.node.data.properties['return_type']
-            
+
             result_dict = {}
 
             try:
@@ -139,7 +143,7 @@ class ExecuteView(execute.Execute):
                 numeric_result = f'{int(numeric_result)}'
             else:
                 numeric_result = int(numeric_result)
-            
+
             if email_field_value is not None:
                 result_dict['email_result'] = email_field_value
             if datetime_field_value is not None:
